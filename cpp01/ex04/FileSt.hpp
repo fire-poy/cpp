6829e4faa02d5c0879d8a8691ed135dd6ffd4b2d
Mauro Pons <mpons@student.42lausanne.ch>
2022-08-20 17:36:25 +0200

    cpp 01 good

--- cpp01/ex04/FileSt.hpp
+++ cpp01/ex04/FileSt.hpp
@@ -6,38 +6,30 @@
 /*   By: mpons <mpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 15:15:58 by mpons             #+#    #+#             */
-/*   Updated: 2022/08/20 13:53:04 by mpons            ###   ########.fr       */
+/*   Updated: 2022/08/20 17:28:14 by mpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILEST_HPP
 # define FILEST_HPP
 
-#include <iostream>
-#include <fstream>
+# include <iostream>
+# include <fstream>
 
 struct FileSt
 {
-	FileSt(void);
-	~FileSt();
-	
-	// file names and streams
-		std::string     inFname;
-		std::string     outFname;
-		std::ifstream   inF;
-		std::ofstream   outF;
-
-		// strings to replace
-		std::string     s1;
-		std::string     s2;
-		std::size_t     l1;
-		std::size_t     l2;
-
-		// buffer manipulations
-		std::string buf;
-		std::string tmp1;
-		std::string tmp2;
-		std::size_t i;
+	std::string     inFname;
+	std::string     outFname;
+	std::ifstream   inF;
+	std::ofstream   outF;
+	std::string     s1;
+	std::string     s2;
+	std::size_t     l1;
+	std::size_t     l2;
+	std::string 	buf;
+	std::string		tmp1;
+	std::string 	tmp2;
+	std::size_t 	i;
 };
 
 #endif
--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,10 @@
 /*   By: mpons <mpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 08:53:04 by mpons             #+#    #+#             */
-/*   Updated: 2022/08/20 14:09:25 by mpons            ###   ########.fr       */
+/*   Updated: 2022/08/20 17:28:09 by mpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <fstream>
 #include "FileSt.hpp"
 
 int	init_f(int ac, char **av, FileSt *f)
@@ -22,29 +20,34 @@ int	init_f(int ac, char **av, FileSt *f)
 		return (1);	
 	}
 	f->inFname = av[1];
-	f->inF.open(av[1]);
-	// f->inF.open(f->inFname);
+	f->inF.open(f->inFname.c_str());
     if (f->inF.fail() == true)
     {
-        std::cout << "error: open file " << f->inFname << std::endl;
+        std::cout << "error: can't open file " << f->inFname << std::endl;
         return (1);
     }
 	f->outFname = av[1];
 	f->outFname += ".replace";
-	// std::cout << f->outFname << std::endl;
-	f->outF.open(f->outFname);
+	f->outF.open(f->outFname.c_str());
     if (f->outF.fail() == true)
     {
         std::cout << "error: creating file " << f->outFname << std::endl;
         return (1);
     }
 	f->s1 = av[2];
+	if (f->s1.empty())
+	{
+        std::cout << "s1 can't be empty" << std::endl;
+        return (1);
+    }
 	f->s2 = av[3];
 	f->l1 = f->s1.length();
     f->l2 = f->s2.length();
     return (0);
 }
 
+// open the file -> filename <- and copies its content into a new file -> filename.replace <-
+// replacing every occurrence of s1 with s2.
 int	main(int ac, char **av)
 {
 	FileSt	f;
@@ -55,15 +58,12 @@ int	main(int ac, char **av)
 	while (getline(f.inF, f.buf))
     {
         i = f.buf.find(f.s1);
-		std::cout << "1st " << i << std::endl;
         while (i != f.buf.npos)
         {
-            f.tmp1 = f.buf.substr(0, i); //copia linea hasta i
-            f.tmp2 = f.buf.substr(i + f.l1); // copia linea desde de i + l1
-            f.buf = f.tmp1 + f.s2 + f.tmp2; //remplaza buf por tmp1 + s2 + tmp2
-            i = f.buf.find(f.s1); // busca siguiente
-			std::cout << "m " << i << std::endl;
-			std::cout << "NPOS" << f.buf.npos << std::endl;
+            f.tmp1 = f.buf.substr(0, i);
+            f.tmp2 = f.buf.substr(i + f.l1);
+            f.buf = f.tmp1 + f.s2 + f.tmp2;
+            i = f.buf.find(f.s1);
         }
         f.outF << f.buf;
         if (f.inF.eof() == false)
@@ -74,32 +74,3 @@ int	main(int ac, char **av)
 	f.outF.close();
 	return (0);
 }
-
-// streambuffer
-// fichier + test
-
-// Forbidden functions : std::string::replace
-// Using C file manipulation functions is forbidden and will be considered cheating.
-
-//  All the member functions of the class std::string are allowed sauf replace
-// handle unexpected inputs and errors
-//  You have to create and turn in your own tests to ensure your program works as expected.
-
-// open the file -> filename <- and copies its content into a new file -> filename.replace <-
-// replacing every occurrence of s1 with s2.
-	
-	// open f1;
-	// search s1;
-	// copies from 0 to pos -1,
-	// append s2,	
-	// open f1;
-// rdbuf
-//     Get stream buffer (public member function)
-// read
-//     Read block of data (public member function)
-// getline
-//     Get line (public member function)
-// rdbuf
-// rdbuf
-//     Get/set stream buffer (public member function)
-	// ofs	 << "i like ponies a whole damm lot" << std::endl
