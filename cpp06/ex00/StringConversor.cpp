41fa658945962c7db66e5f283cef2104aae293f9
mpons <mpons@student.42lausanne.ch>
2022-11-30 16:48:08 +0100

    cpp6 detectType avance

--- cpp06/ex00/StringConversor.cpp
+++ cpp06/ex00/StringConversor.cpp
@@ -17,8 +17,6 @@ StringConversor::StringConversor(std::string const & input) : _input(input)
 	StringConversor::detectConvertType(std::string const input);
 	
 
-
-
 }
 
 StringConversor::~StringConversor()
@@ -58,7 +56,7 @@ bool	StringConversor::specialCase(std::string const input)
 								 std::numeric_limits<double>::quiet_NaN()};
 	for (int i(0); i < 3; i++)
 	{
-		if (!input.compare(floatLimits[i]))
+		if (input == floatLimits[i])
 		{
 			_type = FLOAT;
 			_f = static_cast<float>(limitValues[i]);
@@ -70,7 +68,7 @@ bool	StringConversor::specialCase(std::string const input)
 	}
 	for (int i = 0; i < 3; i++)
 	{
-		if (!input.compare(doubleLimits[i])
+		if (input == doubleLimits[i])
 		{
 			_type = DOUBLE;
 			_f = static_cast<float>(limitValues[i]);
@@ -97,33 +95,54 @@ void	StringConversor::detectConvertType(std::string const input)
 		_c = input.at(0);
 		return true;
 	}
-	// int	i = input.begin();
-	// int	j = 0;
-	// for (i = input.begin(); i != input.end(); i++)
-	// {
-	// 	if (!std::isdigit(input.at(i)))
-	// 	{
-	// 		if (input.at(i) == '+' || input.at(i) == '-') 
-	// 			i++;
-	// 		else
-	// 		{
-	// 			_source = UNKNOWN;
-	// 			return false;
-	// 		}
-	// 	}
-	// 	if (std::isdigit(input.at(i)))
-	// 	{
-	// 		i++;
-	// 		j++;
-	// 		if (input.at(i) && input.at(i) == '.' && std::isdigit(input.at(i + 1)))
-	// 		{
-	// 			if (++point > 1)
-	// 				return (0);
-	// 			i++;
-	// 		}
-	// 	}
 	if (specialCase(input))
-		return;
+		return true;
+	if (!std::isdigit(input.at(i)))
+	{
+		if (input.at(0) == '+' || input.at(0) == '-') 
+			i++;
+		else
+		{
+			_source = UNKNOWN;
+			return false;
+		}
+	}
+	int	i = 0;
+	int	digit = 0;
+	int	point = 0;
+	while (std::isdigit(input.at(i))
+	{
+		digit++;
+		i++;
+		if (input.at(i) && input.at(i) == '.' && std::isdigit(input.at(i + 1)))
+		{
+			if (++point > 1)
+			{
+				_source = UNKNOWN;
+				return false;
+			}
+			i++;
+		}
+	}
+	if (input.at(i) == input.end() && input.at(i) == 'f' && digit > 0 && point == 1)
+	{
+		_source = FLOAT;
+		return true;
+	}
+	if (input.at(i) == std::string::npos && digit > 0 && point == 1)
+	{
+		_source = DOUBLE;
+		return true;
+	}
+	if (input.at(i) == std::string::npos && digit > 0 && point == 0)
+	{
+		_source = INT;
+		return true;
+	}
+	return (0);
+	// dot_point != std::string::npos
+	// for (i = input.begin(); i != input.end(); i++)
+}
 
 }
 
