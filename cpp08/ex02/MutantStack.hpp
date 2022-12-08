b2100c21f619b4a62fd358d095ede38899fd659a
mpons <mpons@student.42lausanne.ch>
2022-12-08 21:16:51 +0100

    cpp finish

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,6 +1,14 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+# define RESET "\033[0m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m" 
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m" 
+
 # include <stack>
 # include <iostream>
 
@@ -17,11 +25,14 @@ class MutantStack: public std::stack<T>
 		MutantStack<T>	&operator=(const MutantStack<T> &rhs)
 		{
 			std::stack<T>::operator=(rhs);
-			// (void)rhs;
-			// return *this;
 		}
 		~MutantStack<T>() {}
-		typedef typename std::stack<T>::container_type::iterator	iterator;
+		
+		typedef typename std::stack<T>::container_type::iterator				iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+		
 		iterator	begin()
 		{
 			return this->c.begin();
@@ -31,18 +42,32 @@ class MutantStack: public std::stack<T>
 			return this->c.end();
 		}
 
-		// typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-		// reverse_iterator rbegin() { return this->c.rbegin(); }
-		// reverse_iterator rend()   { return this->c.rend(); }
+		reverse_iterator	rbegin()
+		{
+			return this->c.rbegin();
+		}
+		reverse_iterator	rend()
+		{
+			return this->c.rend();
+		}
 
-		// typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-		// const_iterator begin() const { return this->c.begin(); }
-		// const_iterator end() const { return this->c.end(); }
+		const_iterator	begin() const
+		{
+			return this->c.begin();
+		}
+		const_iterator	end() const
+		{
+			return this->c.end();
+		}
 
-		// typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
-		// const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-		// const_reverse_iterator rend() const { return this->c.rend(); }
+		const_reverse_iterator	rbegin() const
+		{
+			return this->c.rbegin();
+		}
+		const_reverse_iterator	rend() const
+		{
+			return this->c.rend();
+		}
 };
 
 #endif
-// typedef typename Container::iterator it;
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,62 +1,71 @@
 #include "MutantStack.hpp"
-
-int main()
-{
-	MutantStack<int> mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << "top = " << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << "size = " << mstack.size() << std::endl;
-	std::cout << "top = " << mstack.top() << std::endl;
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
-	}
-	std::stack<int> s(mstack);
-	return 0;
-}
-
-// /*
 #include <iostream>
 
-void	test_function(const MutantStack<int> &ms)
+// int main()
+// {
+// 	MutantStack<int> mstack;
+// 	mstack.push(5);
+// 	mstack.push(17);
+// 	std::cout << "top = " << mstack.top() << std::endl;
+// 	mstack.pop();
+// 	std::cout << "size = " << mstack.size() << std::endl;
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// 	//[...]
+// 	mstack.push(0);
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 	std::cout << *it << std::endl;
+// 	++it;
+// 	}
+// 	std::stack<int> s(mstack);
+// 	return 0;
+// }
+
+void	const_iteration(const MutantStack<int> &m)
 {
-	std::cout << "-------- CONST ITERATOR -------" << std::endl;
-	for(MutantStack<int>::const_iterator it(ms.begin()); it < ms.end(); it++)
-		std::cout << "elem = " << *it << std::endl;
-	std::cout << "-------- REVERSE CONST ITERATOR -------" << std::endl;
-	for(MutantStack<int>::const_reverse_iterator it(ms.rbegin()); it < ms.rend(); it++)
-		std::cout << "elem = " << *it << std::endl;
+	std::cout << GREEN << "Const Iterator\n\n";
+	for (MutantStack<int>::const_iterator it(m.begin()); it < m.end(); it++)
+	{
+		std::cout << *it << std::endl;
+		// *it += 10;
+	}
+	std::cout << "\n" << YELLOW << "Const Reverse\n\n";
+
+	for(MutantStack<int>::const_reverse_iterator it(m.rbegin()); it < m.rend(); it++)
+	{
+		std::cout << *it << std::endl;
+		// *it += 10;
+	}
 }
+
 int main ()
 {
-	MutantStack<int> ms;
-	ms.push(23);
-	ms.push(11);
-	ms.push(10);
-	ms.push(78);
+	MutantStack<int> m;
+	m.push(4);
+	m.push(83);
+	m.push(210);
+	m.push(5601);
 
-	std::cout << "-------- ITERATOR -------" << std::endl;
-	MutantStack<int>::iterator it(ms.begin());
-	MutantStack<int>::iterator ite(ms.end());
-	for (;it < ite; it++)
-		std::cout << "elem = " << *it << std::endl;
-	std::cout << "-------- REVERSE ITERATOR -------" << std::endl;
-	for(MutantStack<int>::reverse_iterator it(ms.rbegin()); it < ms.rend(); it++)
-		std::cout << "elem = " << *it << std::endl;
-	test_function(ms);
+	std::cout << MAGENTA << "Iterator\n\n";
+	for (MutantStack<int>::iterator it(m.begin()); it < m.end(); it++)
+	{
+		std::cout << *it << std::endl;
+		*it *= 10;
+	}
+
+	std::cout << "\n" << RED << "Reverse\n\n";
+	for(MutantStack<int>::reverse_iterator it(m.rbegin()); it < m.rend(); it++)
+	{
+		std::cout << *it << std::endl;
+		*it /= 10;
+	}
+	std::cout << "\n-------------------------\n" << RESET;
+	const_iteration(m);
 	return 0;
 }
-// */
